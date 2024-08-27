f44d4f70d9f823b66ce1f740ff49db5902808212
kmdeck <65979205+kmdeck@users.noreply.github.com>
2024-08-27 13:56:57 -0700

    Remove linear interpolation

--- src/standalone/Soil/energy_hydrology.jl
+++ src/standalone/Soil/energy_hydrology.jl
@@ -715,13 +715,13 @@ end
 """
 function ClimaLand.surface_evaporative_scaling(model::EnergyHydrology{FT}, Y, p) where {FT}
     (; ν, θ_r, d_ds, earth_param_set, hydrology_cm) = model.parameters
-    θ_l_sfc = p.soil.sfc_scratch
-    ClimaLand.Domains.linear_interpolation_to_surface!(
-        θ_l_sfc,
-        p.soil.θ_l,
-        model.domain.fields.z,
-        model.domain.fields.Δz_top,
-    )
+    θ_l_sfc = ClimaLand.top_center_to_surface(p.soil.θ_l)#p.soil.sfc_scratch
+   # ClimaLand.Domains.linear_interpolation_to_surface!(
+   #     θ_l_sfc,
+  #      p.soil.θ_l,
+   #     model.domain.fields.z,
+  #     model.domain.fields.Δz_top,
+   # )
     # These are non-allocating
     θ_i_sfc = ClimaLand.Domains.top_center_to_surface(Y.soil.θ_i)
     hydrology_cm_sfc = ClimaLand.Domains.top_center_to_surface(hydrology_cm)
@@ -747,13 +747,13 @@ function ClimaLand.surface_resistance(
     t,
 ) where {FT}
     (; ν, θ_r, d_ds, earth_param_set, hydrology_cm) = model.parameters
-    θ_l_sfc = p.soil.sfc_scratch
-    ClimaLand.Domains.linear_interpolation_to_surface!(
-        θ_l_sfc,
-        p.soil.θ_l,
-        model.domain.fields.z,
-        model.domain.fields.Δz_top,
-    )
+    θ_l_sfc = ClimaLand.Domains.top_center_to_surface(p.soil.θ_l)#p.soil.sfc_scratch
+   # ClimaLand.Domains.linear_interpolation_to_surface!(
+   #     θ_l_sfc,
+   #     p.soil.θ_l,
+    #    model.domain.fields.z,
+   #     model.domain.fields.Δz_top,
+   # )
     # These are non-allocating
     θ_i_sfc = ClimaLand.Domains.top_center_to_surface(Y.soil.θ_i)
     hydrology_cm_sfc = ClimaLand.Domains.top_center_to_surface(hydrology_cm)
@@ -843,13 +843,13 @@ function ClimaLand.surface_specific_humidity(
     M_w = LP.molar_mass_water(model.parameters.earth_param_set)
     thermo_params =
         LP.thermodynamic_parameters(model.parameters.earth_param_set)
-    ψ_sfc = p.soil.sfc_scratch
-    ClimaLand.Domains.linear_interpolation_to_surface!(
-        ψ_sfc,
-        p.soil.ψ,
-        model.domain.fields.z,
-        model.domain.fields.Δz_top,
-    )
+    ψ_sfc = ClimaLand.Domains.top_center_to_surface(p.soil.ψ)#p.soil.sfc_scratch
+    #ClimaLand.Domains.linear_interpolation_to_surface!(
+    #    ψ_sfc,
+     #   p.soil.ψ,
+     #   model.domain.fields.z,
+      #  model.domain.fields.Δz_top,
+    #)
     q_over_ice =
         Thermodynamics.q_vap_saturation_generic.(
             thermo_params,
